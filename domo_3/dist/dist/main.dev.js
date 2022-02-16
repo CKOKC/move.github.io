1912e2aa59c1cd513020e991e670a5ce4b13e6b3
CKOKC <96469353+CKOKC@users.noreply.github.com>
2022-02-16 17:15:39 +0800

    move1

---
+++ domo_3/dist/dist/main.dev.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var header = document.querySelector('header');
+var menu = document.querySelector('#menu-icon');
+var navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', function () {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+menu.onclick = function () {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+window.onscroll = function () {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+};
+
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+var swiper = new Swiper(".coming-container", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 55000,
+    disabeOnInteraction: false
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2
+    },
+    568: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 4
+    },
+    968: {
+      slidesPerView: 5
+    }
+  }
+});
